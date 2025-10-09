9272ec11bdffb4def32e742de06c924e62845814
info@aayurcare.com <info@aayurcare.com>
2025-10-09 18:08:25 +0530

    commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,94 @@
-import logo from './logo.svg';
-import './App.css';
-
+import "./App.css";
+import Aayurcareicon from "./Images/aayuricon.jpg";
+import Basicplan from "./basicplan"
+import { useNavigate } from "react-router-dom";
+import { Routes,Route} from "react-router-dom";
 function App() {
+    const navigate = useNavigate();
   return (
+    
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {/* Navbar */}
+  
+      <nav className="navbar">
+        <div className="logo">
+          <img src={Aayurcareicon} alt=""/>
+          <span className="brand-name">AayurCare</span>
+        </div>
+      </nav>
+
+      {/* Main Content */}
+      <div className="main-content">
+        {/* Left Section */}
+        <div className="left-section">
+          <header className="header-section">
+            <br></br>
+            <br></br>
+            <br></br>
+            <h1>AayurCare in Partnership with Quess</h1>
+            <p className="subtitle">
+              Exclusive OPD reimbursement for Quess employees
+            </p>
+          </header>
+
+          {/* <section className="benefits">
+            <h2>Benefits</h2>
+            <ul>
+              <li>User-friendly portal</li>
+              <li>Easy OPD reimbursement</li>
+              <li>Quick claim processing</li>
+              <li>Dedicated support team</li>
+            </ul>
+          </section> */}
+
+          <section className="support">
+            <p>Need help? Reach out to our customer support team.</p>
+            <button className="callback-btn">Request a Callback</button>
+            {/* <form className="callback-form">
+              <input type="text" placeholder="Enter your name" required />
+              <input type="email" placeholder="Enter your email" required />
+              <button type="submit">Submit</button>
+            </form> */}
+          </section>
+        </div>
+
+        {/* Right Section */}
+        <div className="right-section">
+          <h2>OPD Reimbursement Plans</h2>
+
+          <div className="plan-card basic">
+            <h3>Basic Plan</h3>
+            <p>₹200 / month</p>
+      
+            <button onClick={()=>navigate("/Basicplan")} >Select Plan</button>
+      
+          </div>
+
+          
+
+          <div className="plan-card premium">
+            <h3>Premium Plan</h3>
+            <p>₹400 / month</p>
+
+            <button>Select Plan</button>
+          </div>
+
+        </div>
+
+      </div>
+       <Routes>
+       <Route path="/Basicplan" element={<Basicplan />} />
+       </Routes>
     </div>
+    
+
+
+
+
+
+
+
+
   );
 }
 
---
+++ src/premiumplan.js
@@ -0,0 +1,92 @@
+import "./App.css";
+
+
+function PremiumPlan() {
+  return (
+    <div className="premiumplan">
+    <h1>Premium plan Bonefits</h1>
+
+
+    <h2>Doctor Consultation & Medical Services</h2>
+    <p>Free monthly teleconsultation with a General Physician</p>
+    <p>Free consultation with Nutritionists and Physiotherapists</p>
+    <p>5 consultations with Mental Health Experts</p>
+    <p>Second opinion for any planned hospitalization</p>
+    <p>Yoga Consultation and Therapy discounts upto 15-30%</p>
+
+
+     <h2>Mental wellness workshops</h2>
+     <p>Access to self-help diagnosis and tools</p>
+     <p> 24/7 emergency helpline</p>
+     <p> Disease risk predictions and mitigation tools</p>
+
+     <h2>Medicines & Pharmacy</h2>
+     <p> 10% discount on medicines</p>
+     <p>Discounts on Ayurveda consultations & medicines</p>
+     
+
+     <h2>Diagnostics & Lab Services</h2>
+     <p>Radiology & Pathology services included</p>
+     <p>Up to 15% discount on lab tests with the customized plan</p>
+     <p> Health Checkups – discounts via Nura Health (Bangalore, Mumbai, Hyderabad, Gurgaon)</p>
+     <p>Gut health test discounts</p>
+
+
+    <h2>Dental & Eye Care</h2>
+    <p>Complimentary basic dental check up</p>
+    <p>10–40% discount on dental procedures at partner clinics (Clove Dental)</p>
+    <p>Complimentary eye check up</p>
+
+
+
+    <h2>Fitness & Wellness</h2>
+    <p>Mental wellness workshops & webinars</p>
+    <p>Discounts upto 15% on Yoga consultation</p>
+
+
+    <h2>Education Loan Benefits</h2>
+    <p>Loan amount: ₹50,000 – ₹20 Lakhs</p>
+    <p> Interest rate from 0%-8.5%.</p>
+    <p>Covers:</p>
+    <p> School fees (up to 12th Class)</p>
+    <p>Undergraduate & Postgraduate programs (India & Abroad)</p>
+    <p> Vocational, Technical Training, ITI, Diploma Courses</p>
+    <p>Certification & Professional Development Courses</p>
+    <p>Online education from recognized institutions</p>
+
+
+    <h2>Other Key Benefits</h2>
+    <p>Ambulance services Pan India – 10% discount</p>
+    <p>Discounts on surgeries & procedures through Pristyn Care</p>
+    <p>Women's health – discounts on consultations, vaccinations, Pap smears</p>
+    <p>Men's health – 1 free consult + discounts</p>
+    <p>Fertility support – free consults + discounts</p>
+
+
+    <h2>Reimbursement Note</h2>
+    <p>Services including doctor consultations, Radiology, Pathology and Medicines are available via reimbursement model.</p>
+
+
+    <h2>Travel Benefits</h2>
+    <p> Up to 25% discount on train tickets</p>
+    <p>Up to 15% discount on bus tickets</p>
+
+
+
+     
+
+
+
+
+
+
+
+
+
+
+
+     </div>
+  );
+}
+
+export default PremiumPlan;
