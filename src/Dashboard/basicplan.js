be620ec1bae87516c09d5aa12011b9d89e514bf1
info@aayurcare.com <info@aayurcare.com>
2025-11-10 22:21:13 +0530

    push code

--- src/Dashboard/basicplan.js
+++ src/Dashboard/basicplan.js
@@ -5,8 +5,8 @@ function Basicplan() {
   // const user = JSON.parse(localStorage.getItem("user"));
 const handleBuyNow = async () => {
   try {
-    const response = await fetch("http://localhost:7000/payment/initiate", {
-    // const response = await fetch("https://api.partner-quess.aayurcare.com/payment/initiate", {
+    // const response = await fetch("http://localhost:7000/payment/initiate", {
+    const response = await fetch("https://api.partner-quess.aayurcare.com/payment/initiate", {
 
       method: "POST",
       headers: { "Content-Type": "application/json" },
