8aa3f03fa17b3e64a6d672cc78a149645849c208
info@aayurcare.com <info@aayurcare.com>
2025-11-11 10:52:07 +0530

    push code

--- src/Dashboard/basicplan.js
+++ src/Dashboard/basicplan.js
@@ -15,7 +15,7 @@ const handleBuyNow = async () => {
         //from api we need to call firstname and gmail 
         firstname: "Kirthana",             
         email: "kirthana@gmail.com",  
-        amount: "1",                    
+        amount: "200",                    
         productinfo: "Basic Health Plan", 
       }),
     });
