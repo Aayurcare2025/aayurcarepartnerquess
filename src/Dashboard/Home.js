bc2815f069be2256a40610470e3accdd2e33051b
kirthana <info@aayurcare.com>
2025-11-17 11:53:52 +0530

    push code

--- src/Dashboard/Home.js
+++ src/Dashboard/Home.js
@@ -6,7 +6,7 @@ import WhatsApp from "../Images/whatsappcion.png"
 
 function Home() {
   const navigate = useNavigate();
-
+//this is home page of quess employees
   return (
     <div className="App">
       <div className="main-content">
