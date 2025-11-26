744a762338a9531fc2681d98b3f06828923779cc
Aayurcare2025 <nambiarkirthana23@gmail.com>
2025-11-26 17:03:54 +0530

    update

--- src/analytics.js
+++ src/analytics.js
@@ -1,7 +1,9 @@
 //analytics.jsimport ReactGA from "react-ga4";
 
 export const initGA = () => {
-  ReactGA.initialize("G-P4512BJZDC");
+  ReactGA.initialize("G-P4512BJZDC"),{
+    debug: true,
+  }
 };
 
 export const trackPage = (path) => {
