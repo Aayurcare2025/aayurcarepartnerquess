c1e3f627a12d56d7770dfe826f1e4523735e0b70
Aayurcare2025 <nambiarkirthana23@gmail.com>
2025-11-26 17:37:57 +0530

    push

--- src/analytics.js
+++ src/analytics.js
@@ -1,12 +1,12 @@
-//analytics.jsimport ReactGA from "react-ga4";
-import ReactGA from "react-ga4";
-export const initGA = () => {
-  ReactGA.initialize("G-P4512BJZDC");
-};
+// //analytics.jsimport ReactGA from "react-ga4";
+// import ReactGA from "react-ga4";
+// export const initGA = () => {
+//   ReactGA.initialize("G-P4512BJZDC");
+// };
 
-export const trackPage = (path) => {
-  ReactGA.send({
-    hitType: "pageview",
-    page: path,
-  });
-};
+// export const trackPage = (path) => {
+//   ReactGA.send({
+//     hitType: "pageview",
+//     page: path,
+//   });
+// };
